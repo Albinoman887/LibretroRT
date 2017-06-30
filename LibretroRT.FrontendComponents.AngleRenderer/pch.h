b1b38044a23ec2727051e08177675728a1656e32
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-06-30 17:11:56 +0900

    Logic

--- LibretroRT.FrontendComponents.AngleRenderer/pch.h
+++ LibretroRT.FrontendComponents.AngleRenderer/pch.h
@@ -22,8 +22,3 @@
 #include <EGL/eglplatform.h>
 
 #include <angle_windowsstore.h>
-
-/*#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective*/
